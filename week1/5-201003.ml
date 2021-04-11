1784983079f24f83f3a7cdd4bd4178d011d48a67
Seiga Ueno <seiga.ueno@gmail.com>
2021-04-11 11:25:55 +0900

    make filter

--- week1/5-201003.ml
+++ week1/5-201003.ml
@@ -10,15 +10,7 @@ let rec append l1 l2 =
   | h::hs -> append (reverse hs) (h::l2);;
 
 let filter f l =
-  let reverse x = 
-    let rec dig res y =
-      match y with
-      | [] -> res
-      | h::hs -> dig (h::res) hs in
-    dig [] x in
-  let rec filter_dig res y =
-    match y with
-    | [] -> res
-    | h::hs -> if f h then filter_dig (h::res) hs else filter_dig res hs in
-  reverse(filter_dig [] l);;
-   
+  match l with
+  | [] -> []
+  | x::xs -> if f x then x::(filter f xs) else filter f xs;;
+
--- week1/6-201003.ml
+++ week1/6-201003.ml
@@ -12,8 +12,20 @@ let append_right a b =
   let f x e = x::e in
   fold_right f a b;;
 
-let append_left a b =
-  let f e x = 
 
+let append_left a b = 
+  let f e x =
+    let reverse l = 
+      let rec dig res y =
+        match y with
+        | [] -> res
+        | h::hs -> dig (h::res) hs in
+      dig [] l in
+    reverse (x::(reverse e)) in
+  fold_left f a b;;
 
-  
+let filter_right g l =
+  let f e x = if g x then x::e else e in
+  fold_right f l [];;
+    
+
